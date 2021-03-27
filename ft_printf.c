44082ad29c154acab0665a2276fb854c9a5acb57
nori <a@a.a>
2021-03-27 19:52:56 +0100

    pft

--- ft_printf.c
+++ ft_printf.c
@@ -1,24 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
 #include <stdarg.h>
 #include <stdint.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
+#ifndef REAL
+# define F ft_printf
+#else 
+# define F printf
+#endif
 
-int precision(char **s, va_list *ap, char *flag)
-{
-	int prec;
-
-	prec = 0;
-	if (**s != '.')
-		return (0);
-	(*s)++;
-	flag['.']++;
-	while ('0' <= **s && **s <= '9')
-		prec = prec * 10 + *(*s)++ - '0';
-	return (prec);
-}
-
-int field_width(char **s, va_list *ap)
+int field_width(char **s)
 {
 	int field;
 
@@ -28,52 +18,66 @@ int field_width(char **s, va_list *ap)
 	return (field);
 }
 
+int precision(char **s, char *flag)
+{
+	int prec;
+
+	if (**s != '.')
+		return (0);
+	(*s)++;
+	flag['.']++;
+	prec = 0;
+	while ('0' <= **s && **s <= '9')
+		prec = prec * 10 + *(*s)++ - '0';
+	return (prec);
+}
+
 void conversion(char **s, char *flag)
 {
 	if (**s == 'd' || **s == 'x' || **s == 's')
 		flag[(int)*(*s)++]++;
 }
 
-int nbr_base(uint32_t n, int base, int len, char *num)
+int put_nbr(uint32_t u, char *num, int base, int len)
 {
-	if (n >= (uint32_t)base)
-		len = nbr_base(n / base, base, len, num);
-	num[len++] = "0123456789abcdef"[n % base];
+	if (u >= (uint32_t)base)
+		len = put_nbr(u / base, num, base, len);
+	num[len++] = "0123456789abcdef"[u % base];
 	return (len);
 }
 
-int itoa_base(int32_t n, char *flag, char *num)
+int itoa_base(int n, char *flag, char *num)
 {
-	int			base;
-	uint32_t	u;
+	uint32_t u;
+	int base;
 
-	base = flag['d'] ? 10 : 16;
-	u = n > 0 || flag['x'] ? n : -n;
-	return (nbr_base(u, base, 0, num));
+	base = flag['x'] ? 16 : 10;
+	u = (flag['x'] || n < 0) ? -n : n;
+	return (put_nbr(u, num, base, 0));
 }
 
-int integer(va_list *ap, int field, int prec, char *flag)
+int integer(va_list *ap, char *flag, int field, int prec)
 {
-	int		n;
-	int		zero;
-	int		digit;
-	int		space;
-	int		len;
-	char	num[30] = {};
+	int n;
+	int digit;
+	int len;
+	int zero;
+	int space;
+	char num[30];
 
 	n = va_arg(*ap, int);
 	digit = itoa_base(n, flag, num);
-	if (!prec && !n && flag['.'])
+	if (!prec && flag['.'] && !n)
 		digit = 0;
 	len = digit;
-	zero = prec > digit ? prec - digit : 0;
-	if (flag['d'] && n < 0)
-		len++;
+	zero = prec > len ? prec - len : 0;
 	len += zero;
-	space = len > field ? 0 : field - len;
+	if (n < 0 && !flag['x'])
+		len++;
+	space = field > len ? field - len : 0;
 	while (space--)
 		write(1, " ", 1);
-	if (flag['d'] && n < 0)
+	if (n < 0 && !flag['x'])
 		write(1, "-", 1);
 	while (zero--)
 		write(1, "0", 1);
@@ -81,18 +85,18 @@ int integer(va_list *ap, int field, int prec, char *flag)
 	return (field > len ? field : len);
 }
 
-int put_str(char *s, int32_t field, int32_t prec, char *flag)
+int put_str(char *s, char *flag, int field, int prec)
 {
-	int	i;
-	int	len;
-	int	space;
+	int space;
+	int len;
+	int i;
 
 	if (!s)
-		return (put_str("(null)", field, prec, flag));
+		return (put_str("(null)", flag, field, prec));
 	i = 0;
 	while (s[i])
 		i++;
-	len = prec < i && flag['.'] ? prec : i;
+	len = i > prec && flag['.'] ? prec : i;
 	space = field > len ? field - len : 0;
 	while (space--)
 		write(1, " ", 1);
@@ -100,32 +104,32 @@ int put_str(char *s, int32_t field, int32_t prec, char *flag)
 	return (field > len ? field : len);
 }
 
-int character(va_list *ap, int32_t field, int32_t prec, char *flag)
+int character(va_list *ap, char *flag, int field, int prec)
 {
 	char *s;
 
 	s = va_arg(*ap, void *);
-	return (put_str(s, field, prec, flag));
+	return (put_str(s, flag, field, prec));
 }
 
 int convert(char **s, va_list *ap)
 {
-	int		field;
-	int		prec;
-	char	flag[150] = {};
+	int field;
+	int prec;
+	char flag[255];
 
-	if (!**s)
+	if (**s != '%')
 		return (0);
 	(*s)++;
-	if (**s == '0' || **s == '-')
+	if (**s == ' ' || **s == '0' || **s == '-')
 		(*s)++;
-	field = field_width(s, ap);
-	prec = precision(s, ap, flag);
+	field = field_width(s);
+	prec = precision(s, flag);
 	conversion(s, flag);
 	if (flag['d'] || flag['x'])
-		return (integer(ap, field, prec, flag));
+		return (integer(ap, flag, field, prec));
 	else if (flag['s'])
-		return (character(ap, field, prec, flag));
+		return (character(ap, flag, field, prec));
 	return (0);
 }
 
@@ -139,7 +143,7 @@ int till_per(char **s)
 	return (write(1, tmp, *s - tmp));
 }
 
-int print_va(char *s, va_list *ap)
+int print_va(va_list *ap, char *s)
 {
 	int ret;
 
@@ -152,18 +156,37 @@ int print_va(char *s, va_list *ap)
 	return (ret);
 }
 
-int ft_printf(const char *format, ...)
+int ft_printf(char *format, ...)
 {
-	int		ret;
 	va_list ap;
+	int		ret;
 
 	va_start(ap, format);
-	ret = print_va((char *)format, &ap);
+	ret = print_va(&ap, format);
 	va_end(ap);
 	return (ret);
 }
-
 // int main()
 // {
-// 	ft_printf("%d", -1);
+// 	// int i = 0;
+
+// 	// while (i++ < 100)
+// 	// {
+// 	// 	printf("%d\n", F("%10d\n", i));
+// 	// 	printf("%d\n", F("%10x\n", i));
+// 	// 	printf("%d\n", F("%.10d\n", i));
+// 	// 	printf("%d\n", F("%10.10d\n", i));
+// 	// }
+// 	// while (i-- > -100)
+// 	// {
+// 	// 	printf("%d\n", F("%10d\n", i));
+// 	// 	printf("%d\n", F("%10x\n", i));
+// 	// 	printf("%d\n", F("%.10d\n", i));
+// 	// 	printf("%d\n", F("%10.10d\n", i));
+// 	// }
+// 		// printf("%d\n", ft_printf("%10s\n", "aiueo"));
+// 		// printf("%d\n", ft_printf("%s\n", "aiueo"));
+// 		printf("%d\n", F("%10s\n", "aiueoaiueo"));
+// 		printf("%d\n", F("%.10s\n", "aiueo"));
+// 		printf("%d\n", F("%10.10s\n", "aiueoaiueoa"));
 // }
