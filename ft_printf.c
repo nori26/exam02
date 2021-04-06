5dfa370d839068860cc69708f1404657f43d18e6
nori <a@a.a>
2021-04-06 16:08:47 +0200

    a

--- ft_printf.c
+++ ft_printf.c
@@ -1,142 +1,154 @@
-#include <libc.h>
-#define PC(c) ret += ft_putchar(c)
-#define WRITE_W while (0 < w--){PC(' ');}
-#define WRITE_STR(s) while(*s){PC(*s++);}
-#define WRITE_NEG if (neg){PC('-');}
-#define ATOI(n) while('0' <= *f && *f <= '9'){n = n * 10 + *f++ - '0';}
-#define BREAK if(!*f){break;}
+#include <stdarg.h>
+#include <stdint.h>
+#include <unistd.h>
 
-int  ft_strlen(char *s)
+int field_width(char **s)
 {
-	int i = 0;
-	while (s[i])
-		i++;
-	return i;
+	int field = 0;
+
+	while ('0' <= **s && **s <= '9')
+		field = field * 10 + *(*s)++ - '0';
+	return (field);
 }
 
-int ft_putchar(int c)
+int precision(char **s, char *flag)
 {
-	unsigned char uc;
+	int prec = 0;
 
-	uc = (unsigned char)c;
-	return write(1, &uc, 1);
+	if (**s != '.')
+		return (0);
+	(*s)++;
+	flag['.']++;
+	while ('0' <= **s && **s <= '9')
+		prec = prec * 10 + *(*s)++ - '0';
+	return (prec);
 }
 
-int doprint(va_list ap, const char *f)
+void conversion(char **s, char *flag)
 {
-	int ret = 0;
+	if (**s == 'd' || **s == 'x' || **s == 's')
+		flag[(int)*(*s)++]++;
+}
 
-	while (*f)
+int nbr(uint32_t u, uint32_t base, char *num, int len)
+{
+	if (u >= base)
+		len = nbr(u / base, base, num, len);
+	num[len++] = "0123456789abcdef"[u % base];
+	return (len);
+}
+
+int itoa_base(int n, char *num, char *flag)
+{
+	uint32_t u;
+	uint32_t base;
+
+	base = 10 + 6 * flag['x'];
+	u = flag['x'] || n > 0 ? n : -n;
+	return (nbr(u, base, num, 0));
+}
+
+int integer(va_list *ap, char *flag, int prec, int field)
+{
+	int n;
+	int len;
+	int zero;
+	int space;
+	int digit;
+	char num[30] = {};
+
+	n = va_arg(*ap, int);
+	digit = itoa_base(n, num, flag);
+	if (flag['.'] && !prec && !n)
+		digit = 0;
+	zero = prec > digit ? prec - digit : 0;
+	len = digit + zero;
+	if (n < 0 && flag['d'])
+		len++;
+	space = field > len ? field - len : 0;
+	while (space--)
+		write(1, " ", 1);
+	if (n < 0 && flag['d'])
+		write(1, "-", 1);
+	while (zero--)
+		write(1, "0", 1);
+	write(1, num, digit);
+	return (field > len ? field : len);
+}
+
+int put_str(char *s, int prec, int field, char *flag)
+{
+	int space;
+	int len;
+
+	if (!s)
+		return (put_str("(null)", prec, field, flag));
+	len = 0;
+	while (s[len])
+		len++;
+	len = flag['.'] && prec < len ? prec : len;
+	space = field > len ? field - len : 0;
+	while (space--)
+		write(1, " ", 1);
+	write(1, s, len);
+	return (field > len ? field : len);
+}
+
+int character(va_list *ap, char *flag, int prec, int field)
+{
+	char *s;
+
+	s = va_arg(*ap, void *);
+	return (put_str(s, prec, field, flag));
+}
+
+int convert(char **s, va_list *ap)
+{
+	int field;
+	int prec;
+	char flag[255] = {};
+
+	while (**s == '%' || **s == ' ' || **s == '-' || **s == '0')
+		(*s)++;
+	field = field_width(s);
+	prec = precision(s, flag);
+	conversion(s, flag);
+	if (flag['d'] || flag['x'])
+		return (integer(ap, flag, prec, field));
+	else if (flag['s'])
+		return (character(ap, flag, prec, field));
+	return (0);
+}
+
+int till_per(char **s)
+{
+	char *tmp = *s;
+
+	while (**s && **s != '%')
+		(*s)++;
+	return (write(1, tmp, *s - tmp));
+}
+
+int print_va(va_list *ap, char *fmt)
+{
+	int ret;
+	
+	ret = 0;
+	while (*fmt)
 	{
-		int w = 0;
-		int prec = -1;
-		while (*f && *f != '%')
-			PC(*f++);
-		BREAK;
-		if (*f == '%' && *(f+1))
-		{
-			f++;
-			ATOI(w);
-			BREAK;
-			if (*f == '.')
-			{
-				f++;
-				prec = 0;
-				ATOI(prec);
-				BREAK;
-			}
-			if (*f == 's')
-			{
-				char *s;
-				s = va_arg(ap, char *);
-				if (!s)
-					s = "(null)";
-				int len = ft_strlen(s);
-				if (prec == -1)
-				{
-					w -= len;
-					WRITE_W;
-					WRITE_STR(s);
-				}
-				else
-				{
-					w -= MIN(len, prec);
-					WRITE_W;
-					while (0 < prec-- && *s)
-					{
-						PC(*s++);
-					}
-				}
-			}
-			if (*f == 'd' || *f == 'x')
-			{
-				int neg = 0;
-				int base = 10;
-				intmax_t n;
-				char buf[100] = {0};
-				char *p;
-				p = buf;
-				p++;
-
-				if (*f == 'd')
-				{
-					n = va_arg(ap, int);
-					if (n < 0)
-					{
-						neg = 1;
-						n *= -1;
-					}
-				}
-				else
-				{
-					n = va_arg(ap, unsigned int);
-					base = 16;
-				}
-				while (1)
-				{
-					*p = "0123456789abcdef"[n % base];
-					n /= base;
-					if (n == 0)
-						break;
-					p++;
-				}
-				int len = p - buf;
-				if (prec == 0 && *p == '0')
-				{
-					*p = 0;
-					w++;
-				}
-				if (prec == -1)
-				{
-					w -= len;
-					WRITE_W;
-					WRITE_NEG;
-				}
-				else
-				{
-					w -= MAX(len, prec);
-					prec -= len;
-					WRITE_W;
-					WRITE_NEG;
-					while (0 < prec--)
-						PC('0');
-				}
-				while(*p)
-					PC(*p--);
-			}
-		}
-		f++;
+		ret += till_per(&fmt);
+		ret += convert(&fmt, ap);
 	}
-	return ret;
+	return (ret);
 }
 
-int ft_printf(const char *fmt, ...)
+int ft_printf(char *fmt, ...)
 {
-	int ret = 0;
+	int ret;
 	va_list ap;
 
 	va_start(ap, fmt);
-	ret = doprint(ap, fmt);
+	ret = print_va(&ap, fmt);
 	va_end(ap);
-	return ret;
+	return (ret);
 }
